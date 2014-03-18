fc1cf8017c9f25c1772c1a455575806b6db59999
AndrS <andreas@fromea.se>
2014-03-18 12:06:47 +0000

    added comments to create_symoliclinks

--- create_symboliclinks.sh
+++ create_symboliclinks.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-
+# Creates symbolic links from sourcedirectory in destinationdirectory
+# Searches only in root of sourcedirectory according to maxdepth
 SOURCEDIR=$1
 DESTDIR=$2
 
 mkdir -p "$DESTDIR"
 cd -- "$SOURCEDIR"
 
-#find -maxdepth 1 -type d -exec ln --symolic  -- "$DESTDIR"/{} \;
-
+# Search all files, could be extended to us -type f for files or -type d for directories
 find * -maxdepth 0 -exec ln --symbolic -- "$SOURCEDIR"/{} "$DESTDIR"/{} \;
 
 
