12f3af2f8b567cafbec3de474b32fdbfbbba9b95
Andreateras <andreas@sateras.se>
2015-01-27 12:18:33 +0100

    added new scripts setup_user_environment and update_platform_symbolic_links

---
+++ setup_user_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -L $0 ] ; then
+    SCRIPT_DIR=$(dirname $(readlink -f $0)) ;
+else
+    SCRIPT_DIR=$(dirname $0) ;
+fi ;
+CALLING_DIR=`pwd`
+
+echo $SCRIPT_DIR
+echo $CALLING_DIR
+
+
+ln -s $SCRIPT_DIR/.bash_aliases $CALLING_DIR/.bash_aliases
+ln -s $SCRIPT_DIR/.vimrc $CALLING_DIR/.vimrc
+ln -s $SCRIPT_DIR/.screenrc $CALLING_DIR/.screenrc
