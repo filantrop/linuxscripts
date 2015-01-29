30a56eb1558e99c9943a6dc6efa87400da973819
Andreateras <andreas@sateras.se>
2015-01-29 10:49:35 +0100

    added creating .bashrc and reloading it

--- setup_user_environment.sh
+++ setup_user_environment.sh
@@ -13,3 +13,6 @@ echo $CALLING_DIR
 ln -s $SCRIPT_DIR/.bash_aliases $CALLING_DIR/.bash_aliases
 ln -s $SCRIPT_DIR/.vimrc $CALLING_DIR/.vimrc
 ln -s $SCRIPT_DIR/.screenrc $CALLING_DIR/.screenrc
+ln -s $SCRIPT_DIR/.bashrc $CALLING_DIR/.bashrc
+
+source $CALLING_DIR/.bashrc
